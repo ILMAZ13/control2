d438d8d81864891f644f2ad82e2a7b2f0c755230
ILMAZ13 <ilmaz1309@gmail.com>
2016-11-25 10:10:06 +0300

    user added

--- src/User.java
+++ src/User.java
@@ -1,7 +1,32 @@
-package PACKAGE_NAME;
-
 /**
- * Created by ilmaz on 15.12.16.
+ * Created by ilmaz on 25.11.16.
  */
 public class User {
+    private String name;
+    private String surName;
+    private String number;
+    private boolean bool;
+
+    public User(String name, String surName, String number, boolean bool) {
+        this.name = name;
+        this.surName = surName;
+        this.number = number;
+        this.bool = bool;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public boolean isBool() {
+        return bool;
+    }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
 import java.awt.*;
+import java.util.LinkedList;
 
 /**
  * Created by ilmaz on 25.11.16.
@@ -9,6 +10,11 @@ public class Window implements Runnable {
     private JFrame frame;
     private JPanel panel;
     private JTable table;
+    private java.util.List<User> users;
+    private JTextField nameField;
+    private JTextField surnameField;
+    private JTextField numberField;
+    private JCheckBox jCheckBox;
 
     @Override
     public void run() {
@@ -21,26 +27,32 @@ public class Window implements Runnable {
     }
 
     private void initElements() {
+        users = new LinkedList<>();
         panel = new JPanel(new GridLayout(20, 2));
         panel.add(new JLabel("Name:"));
-        JTextField nameField = new JTextField(15);
+        nameField = new JTextField(15);
         panel.add(nameField);
         panel.add(new JLabel("Surname:"));
-        JTextField surnameField = new JTextField(15);
+        surnameField = new JTextField(15);
         panel.add(surnameField);
         panel.add(new JLabel("Number:"));
-        JTextField numberField = new JTextField(15);
+        numberField = new JTextField(15);
         panel.add(numberField);
         panel.add(new JLabel("Bool:"));
-        JCheckBox jCheckBox = new JCheckBox();
+        jCheckBox = new JCheckBox();
         panel.add(jCheckBox);
         JButton button = new JButton("Save");
+        button.addActionListener(actionEvent -> {
+            User user = new User(nameField.getText(), surnameField.getText(), numberField.getText(), jCheckBox.isSelected());
+            users.add(user);
+            table.updateUI();
+        });
         panel.add(button);
         frame.add(panel, BorderLayout.EAST);
         table = new JTable(new AbstractTableModel() {
             @Override
             public int getRowCount() {
-                return 1;
+                return users.size();
             }
 
             @Override
@@ -50,7 +62,18 @@ public class Window implements Runnable {
 
             @Override
             public Object getValueAt(int i, int i1) {
-                return null;
+                switch (i1) {
+                    case 0:
+                        return users.get(i).getName();
+                    case 1:
+                        return users.get(i).getSurName();
+                    case 2:
+                        return users.get(i).getNumber();
+                    case 3:
+                        return users.get(i).isBool();
+                    default:
+                        return null;
+                }
             }
         });
         table.setRowHeight(30);
